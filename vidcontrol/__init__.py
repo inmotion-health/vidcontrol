b87c74ad8f46b7eb2f95d5ec9ea2a999f10bf7c0
Fabian Klinke <fabian.s.klinke@icloud.com>
2023-10-12 12:12:26 +0200

    chore: setup package module structure

--- vidcontrol/__init__.py
+++ vidcontrol/__init__.py
@@ -2,6 +2,6 @@ __version__ = "0.1.0"
 
 from .video_source import VideoSource
 from .video_manager import VideoManager
-from .video_platform import VideoPlatform
+from .video_platform import video_platform as VideoPlatform
 
 __all__ = ["VideoSource", "VideoManager", "VideoPlatform"]
