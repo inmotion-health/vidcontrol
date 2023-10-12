059618b19da3d5eca2810fd3144760373c6e2779
Fabian Klinke <fabian.s.klinke@icloud.com>
2023-10-12 13:27:16 +0200

    chore: removed video_platform from public interface

--- vidcontrol/__init__.py
+++ vidcontrol/__init__.py
@@ -2,6 +2,5 @@ __version__ = "0.1.0"
 
 from .video_source import VideoSource
 from .video_manager import VideoManager
-from .video_platform import video_platform as VideoPlatform
 
-__all__ = ["VideoSource", "VideoManager", "VideoPlatform"]
+__all__ = ["VideoSource", "VideoManager"]
