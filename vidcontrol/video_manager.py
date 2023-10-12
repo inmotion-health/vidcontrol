0b384811ba5e08acbf3de1550e9b3982b1124242
Fabian Klinke <fabian.s.klinke@icloud.com>
2023-10-12 17:42:56 +0200

    fix: fixed an issue with the recreation of video sources after prefered height changes

--- vidcontrol/video_manager.py
+++ vidcontrol/video_manager.py
@@ -10,21 +10,13 @@ class VideoManager:
 
     preferred_height = 480
 
-    def create_source(self, camera_id: int):
-        resolution, fps = video_platform.get_resolution_for(camera_id, self.preferred_height)
-        log.debug(f"Using resolution {resolution} and fps {fps} for camera {camera_id}")
-
-        VideoManager._vid_sources = {camera_id: VideoSource(camera_id, resolution, fps)}
-
-        log.info(f"Created reader for camera {camera_id}")
-
     def get_video_source(self, camera_id) -> VideoSource:
         # Check if the reader already exists
         if camera_id in VideoManager._vid_sources:
             return VideoManager._vid_sources[camera_id]
 
         # Create the reader
-        self.create_source(camera_id)
+        self._create_source(camera_id)
 
         return VideoManager._vid_sources[camera_id]
 
@@ -32,4 +24,17 @@ class VideoManager:
         return video_platform.list_video_devices()
 
     def set_preferred_height(self, height: int):
-        self.preferred_height = height
+        VideoManager.preferred_height = height
+        self._recreate_existing_sources()
+
+    def _create_source(self, camera_id: int):
+        resolution, fps = video_platform.get_resolution_for(camera_id, VideoManager.preferred_height)
+        log.debug(f"Using resolution {resolution} and fps {fps} for camera {camera_id}")
+
+        VideoManager._vid_sources = {camera_id: VideoSource(camera_id, resolution, fps)}
+
+        log.info(f"Created reader for camera {camera_id}")
+
+    def _recreate_existing_sources(self):
+        for camera_id in VideoManager._vid_sources:
+            self._create_source(camera_id)
