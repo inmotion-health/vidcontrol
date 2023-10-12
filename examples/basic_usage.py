c24670b9ca26b129dafb88a811620b3ede0ac989
Fabian Klinke <fabian.s.klinke@icloud.com>
2023-10-12 12:23:44 +0200

    doc: added recommended error check to example

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -9,8 +9,13 @@ print(webcams)
 # set the height of the video capture
 manager.set_preferred_height(480)
 
-# get a video source and start capturing
-source = manager.get_video_source(0)
+# get a video source and check for errors
+try:
+    source = manager.get_video_source(0)
+except Exception as e:
+    print(f"An error occurred: {e}")
+    exit(1)
+
 for frame in source:
     # do something with the frame
     pass
