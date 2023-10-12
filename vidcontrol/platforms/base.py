afe79096e9267a5c1df480969df18c9be037a44b
Fabian Klinke <fabian.s.klinke@icloud.com>
2023-10-12 12:51:11 +0200

    feat: created some barebones functions and moved preferred height setting to video manager

--- vidcontrol/platforms/base.py
+++ vidcontrol/platforms/base.py
@@ -1,8 +1,6 @@
 from abc import abstractmethod
 from typing import Dict, Optional, Tuple, List
 
-PREFERRED_HEIGHT = 480
-
 
 class VideoPlatform:
     @abstractmethod
@@ -17,23 +15,18 @@ class VideoPlatform:
     def list_available_resolutions(self, device_id: int) -> Optional[List[Tuple[Tuple[int, int], int]]]:
         pass
 
-    @staticmethod
-    def set_preferred_height(height: int):
-        global PREFERRED_HEIGHT
-        PREFERRED_HEIGHT = height
-
     @abstractmethod
     def get_ffmpeg_device_name(self, cam_id: int) -> str:
         pass
 
-    def get_resolution_for(self, camera_id: int) -> Optional[Tuple[Tuple[int, int], int]]:
+    def get_resolution_for(self, camera_id: int, preferred_height: int) -> Optional[Tuple[Tuple[int, int], int]]:
         resolutions = self.list_available_resolutions(camera_id)
 
         if not resolutions:
             return None
 
         # Find 720p resolutions
-        _720_resolutions = [res for res in resolutions if res[0][1] == PREFERRED_HEIGHT]
+        _720_resolutions = [res for res in resolutions if res[0][1] == preferred_height]
 
         if not _720_resolutions:
             return resolutions[0][0], resolutions[0][1]
--- vidcontrol/video_manager.py
+++ vidcontrol/video_manager.py
@@ -1,8 +1,27 @@
 import vidcontrol.video_platform as video_platform
+from vidcontrol.video_source import VideoSource
 
 
 class VideoManager:
     _readers = {}
 
+    preferred_height = 480
+
     def __init__(self):
         self.platform = video_platform.get_platform()
+
+    def create_readers(self):
+        webcam_list = self.platform.list_video_devices()
+        cam_ids = [cam.id for cam in webcam_list]
+
+        for cam_id in cam_ids:
+            print("Creating reader for cam_id: " + cam_id)
+
+    def get_video_source(self, cam_id) -> VideoSource:
+        raise NotImplementedError("get_video_source is not implemented")
+
+    def list_available_cameras(self):
+        return self.platform.list_video_devices()
+
+    def set_preferred_height(self, height: int):
+        self.preferred_height = height
